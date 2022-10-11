811f520c894307dcc1a482af0c0a951363cb1238
Kevin Price <collegekevin@gmail.com>
2022-10-11 10:34:49 -0700

    commit tree e55f9675a08e233ace62368ca3f437947c1f04c3

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const fare = 10
+
+//const returnFirstTwoDrivers = () => drivers.slice(0, 2);
+ //   returnFirstTwoDrivers();
+
+const returnFirstTwoDrivers = function(allDrivers){
+   return allDrivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = (allDrivers) => allDrivers.slice(-2);
+
+
+    returnLastTwoDrivers(drivers);
+
+//const returnLastTwoDrivers = function(){
+//    allDrivers.slice(0, 2);
+//    returnLastTwoDrivers;
+//}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function multiplyFares (fare, fareMultiplier){
+    totalFare = fare * fareMultiplier;
+    return totalFare;
+}
+
+
+function createFareMultiplier(fareMultiplier) {
+    // if (fareMultiplier === 2) {
+    //     const fareDoubler = (fare) => fare * 2;
+        return  multiplyFares(fare, fareMultiplier)
+}
+
+    // else if (fareMultiplier === 3) {
+    //     const fareTripler = (fare) => fare * 3;
+    //     return fareTripler;
+    // }
+    // else if (fareQuintupler === 4) {
+    //     const fareQuintupler = (fare) => fare * 4;
+    //     return fareQuintupler;
+    // }
+
+
+
+function selectDifferentDrivers(drivers, whichDrivers){
+    // if (whichDrivers === returnFirstTwoDrivers) {
+    //     returnFirstTwoDrivers(drivers);
+    // }
+    // else if (whichDrivers === returnLastTwoDrivers) {
+    //     returnLastTwoDrivers(drivers);
+    // }
+    whichDrivers(drivers)
+
+}
+
+selectDifferentDrivers(drivers, returnFirstTwoDrivers);
+console
